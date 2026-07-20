43ab7f2dc36ab61f37ff5bc8886836811b15d7d0
Ohad Avnery <ohad.avnery@gmail.com>
2026-07-20 14:37:18 +0100

    finish C02

--- MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
+++ MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
@@ -150,7 +150,11 @@ variable (x y z : α)
 -- ohad: from this point,
 -- I'll use comm and assoc because I proved them
 
--- TODO - EXERCISE: build in Lean an explicit example of a non-distributive lattice!
+-- EXERCISE: build a non-distributive lattice
+-- ANSWER: by brute force, the smallest example has 5 elements: 0<a,b,c<1, where each pair of a,b,c has
+-- a join of 1 and a meet of 0.
+-- TODO: construct this example in Lean explicitly. Prove that it's a lattice
+-- and that ¬distributive holds.
 
 #check (inf_sup_left x y z : x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z)
 #check (inf_sup_right x y z : (x ⊔ y) ⊓ z = x ⊓ z ⊔ y ⊓ z)
@@ -167,8 +171,12 @@ example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z) : a ⊔ b �
   --#search "for real X,Y, if X=Y then Y=X."
   let h' : (a ⊔ b) ⊓ (a ⊔ c) =  a ⊔ b ⊓ c := by calc
     (a⊔b)⊓(a⊔c) = ((a⊔b)⊓a) ⊔ ((a⊔b)⊓c) := by apply h
-    _ = a ⊔ (a ⊔ b) ⊓ c := by sorry -- rw [sup_comm a b, absorb1]
-  sorry
+    _ = a ⊔ (a ⊔ b) ⊓ c := by rw [inf_comm (a⊔b) a, absorb1 a b]
+    _ = a ⊔ (c ⊓ (a ⊔ b)) := by rw [inf_comm]
+    _ = a ⊔ ((c ⊓ a) ⊔ (c ⊓ b)) := by rw [h]
+    _ = (a ⊔ (c ⊓ a)) ⊔ (c ⊓ b) := by rw [← sup_assoc]
+    _ = a ⊔ b ⊓ c := by rw [inf_comm c a, absorb2, inf_comm]
+  rw [h']
 
   -- apply le_antisymm
   -- -- ≤ is always true, even without h
@@ -185,7 +193,13 @@ example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z) : a ⊔ b �
 
 
 example (h : ∀ x y z : α, x ⊔ y ⊓ z = (x ⊔ y) ⊓ (x ⊔ z)) : a ⊓ (b ⊔ c) = a ⊓ b ⊔ a ⊓ c := by
-  sorry
+  rw [h]
+  rw [sup_comm (a⊓b) a]
+  rw [sup_comm (a⊓b) c]
+  rw [absorb2]
+  rw [h c]
+  rw [← inf_assoc, sup_comm c a, absorb1, sup_comm c b]
+
 
 end
 
@@ -196,16 +210,46 @@ variable (a b c : R)
 #check (add_le_add_right : a ≤ b → ∀ c, c + a ≤ c + b)
 #check (mul_pos : 0 < a → 0 < b → 0 < a * b)
 
+-- TODO: return to this after chapter 3
 #check (mul_nonneg : 0 ≤ a → 0 ≤ b → 0 ≤ a * b)
 
+#search "a - a = 0 for all a." --sub_self
+#search "a - b = a + (-b)." --sub_eq_add_neg
 example (h : a ≤ b) : 0 ≤ b - a := by
-  sorry
+  -- let ha : a - a = 0 := by apply sub_self
+  rw [← sub_self a]
+  repeat rw [sub_eq_add_neg, add_comm _ (-a)]
+  apply add_le_add_right
+  exact h
 
+#search "-a + a = 0."
 example (h: 0 ≤ b - a) : a ≤ b := by
-  sorry
+  calc
+    a = a + 0 := by abel
+    _ ≤ a + (b - a) := by apply add_le_add_right h
+    _ = b := by abel
+  -- a = a + 0 := by exact (add_zero a).symm -- revser a+0=a
+  -- _ = 0 + a := by exact add_comm a 0
+  -- _ ≤ (b - a) + a := by group
+  -- _  = a + (-a + a) := by rw [neg_add_cancel]
+  -- _ = a - a + a := by rw [←add_assoc, sub_eq_add_neg]
 
+#search "(x-y)*z=x*z-y*z." -- didn't work
+#search "-(x*y) = (-x)*y."
 example (h : a ≤ b) (h' : 0 ≤ c) : a * c ≤ b * c := by
-  sorry
+  have h1 : 0 ≤ b - a := by -- copying previous solution
+    rw [← sub_self a]
+    repeat rw [sub_eq_add_neg, add_comm _ (-a)]
+    apply add_le_add_right
+    exact h
+  have h2 : 0≤(b-a)*c := by apply mul_nonneg h1 h'
+  calc
+    b * c = b * c + -(a * c) + a * c := by abel
+    _ = b * c + ((-a) * c) + a * c := by rw [neg_mul]
+    _ = (b - a) * c + a * c := by rw [← add_mul, sub_eq_add_neg]
+    _ = a * c + (b - a) * c := by abel
+    _ ≥ a * c + 0 := by apply add_le_add_right h2
+    _ = a * c := by abel
 
 end
 
@@ -218,6 +262,12 @@ variable (x y z : X)
 #check (dist_triangle x y z : dist x z ≤ dist x y + dist y z)
 
 example (x y : X) : 0 ≤ dist x y := by
-  sorry
+  have h : 0 ≤ (dist x y) * 2 := by
+    calc
+      0 = dist x x := by exact (dist_self x).symm
+      _ ≤ dist x y + dist y x := by apply dist_triangle
+      _ = dist x y + dist x y := by rw [dist_comm]
+      _ = (dist x y) * 2 := by ring
+  linarith
 
 end
