4355cfc7797de6651aa6cb2d4884e1f52f13ac49
Ohad Avnery <ohad.avnery@gmail.com>
2026-07-21 17:46:42 +0100

    chapter 3 - get up to sets

--- AGENTS.md
+++ AGENTS.md
@@ -1,5 +1,5 @@
 # Project
 
-This repository contains exercises and solutions from the "mathematics in Lean" book. I (Ohad) write my solutions in "MIL copy", separate from the original MIL directory.
-Do not read or edit files unless asked to. This repository is mostly used for me to understand the language, so I'm trying to do things on my own.
+This repository contains exercises and solutions from the "mathematics in Lean" book. I write my solutions in "MIL copy", separate from the original MIL directory.
+Do not read or edit files unless asked to. This repository is mostly used for me to understand the language and not for automation.
 I am running mathlib v4.30.0.
--- MIL/C03_Logic/S01_Implication_and_the_Universal_Quantifier.lean
+++ MIL/C03_Logic/S01_Implication_and_the_Universal_Quantifier.lean
@@ -38,14 +38,22 @@ theorem my_lemma3 :
   intro x y ε epos ele1 xlt ylt
   sorry
 
+
+-- note: there's a problem with the lean server occasionally,
+-- and then the process gets stuck on #search, should comment them out in this case
+-- #search "if a<b then a≤b." -- le_of_lt
+--#search "x≤x for all x." -- le_refl
+
+-- the book solution uses "by apply", and a lot of linarith, so they make it a bit simpler -
+-- no need for all the specific results I used (like a<b≤c -> a<c)
 theorem my_lemma4 :
     ∀ {x y ε : ℝ}, 0 < ε → ε ≤ 1 → |x| < ε → |y| < ε → |x * y| < ε := by
   intro x y ε epos ele1 xlt ylt
   calc
-    |x * y| = |x| * |y| := sorry
-    _ ≤ |x| * ε := sorry
-    _ < 1 * ε := sorry
-    _ = ε := sorry
+    |x * y| = |x| * |y| := abs_mul x y
+    _ ≤ |x| * ε := mul_le_mul (le_refl |x|) (le_of_lt ylt) (abs_nonneg y) (abs_nonneg x)
+    _ < 1 * ε := mul_lt_mul_of_pos_right (lt_of_lt_of_le xlt ele1) epos
+    _ = ε := one_mul ε
 
 def FnUb (f : ℝ → ℝ) (a : ℝ) : Prop :=
   ∀ x, f x ≤ a
@@ -63,15 +71,29 @@ example (hfa : FnUb f a) (hgb : FnUb g b) : FnUb (fun x ↦ f x + g x) (a + b) :
   apply hfa
   apply hgb
 
-example (hfa : FnLb f a) (hgb : FnLb g b) : FnLb (fun x ↦ f x + g x) (a + b) :=
-  sorry
+example (hfa : FnLb f a) (hgb : FnLb g b) : FnLb (fun x ↦ f x + g x) (a + b) := by
+  intro x
+  dsimp
+  --have hf : (f x ≥ a) := by apply hfa
+  --have hg : (g x ≥ b) := by apply hgb
+  --linarith
+  -- alternative option: give hfa, hgb as explicit arguments to linarith
+  linarith [hfa x, hgb x]
 
-example (nnf : FnLb f 0) (nng : FnLb g 0) : FnLb (fun x ↦ f x * g x) 0 :=
-  sorry
+example (nnf : FnLb f 0) (nng : FnLb g 0) : FnLb (fun x ↦ f x * g x) 0 := by
+  intro x
+  dsimp
+  --apply mul_nonneg
+  --apply nnf; apply nng
+  apply mul_nonneg (nnf x) (nng x)
 
+-- f≤a, 0≤g≤b, a≥0 → fg≤ab
 example (hfa : FnUb f a) (hgb : FnUb g b) (nng : FnLb g 0) (nna : 0 ≤ a) :
-    FnUb (fun x ↦ f x * g x) (a * b) :=
-  sorry
+    FnUb (fun x ↦ f x * g x) (a * b) := by
+  intro x
+  dsimp
+  apply mul_le_mul
+  repeat linarith [hfa x, hgb x, nng x] -- a bit overkill but whatever
 
 end
 
@@ -95,19 +117,37 @@ section
 variable (f g : ℝ → ℝ)
 
 example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f x + g x := by
-  intro a b aleb
+  intro a b aleb -- three params to monotone f: a, b, and the statement a≤b
   apply add_le_add
   apply mf aleb
   apply mg aleb
 
+-- ohad - random thought: if we include ∀x:α P(x), that's the same as building a function that
+-- for each x∈α constructs a proof of P(α). That matches the term notation, as x itself is a proof
+-- of x∈α.
+
+
 example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f x + g x :=
   fun _a _b aleb ↦ add_le_add (mf aleb) (mg aleb)
 
+
+#search "if a≤b and c>0 then ca≤cb."
+#check mul_le_mul_of_nonneg_left
+-- ↦ = \maps
 example {c : ℝ} (mf : Monotone f) (nnc : 0 ≤ c) : Monotone fun x ↦ c * f x :=
-  sorry
+  --fun a b aleb ↦ mul_le_mul (by linarith) (mf aleb) sorry nnc
+  -- _a,_b start with underline because they're not used, otherwise it'll cause a warning
+  fun _a _b aleb ↦ mul_le_mul_of_nonneg_left (mf aleb) nnc
 
 example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f (g x) :=
-  sorry
+  fun _a _b aleb ↦ mf (mg aleb)
+
+-- alternative proof
+example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f (g x) := by
+  intro a b aleb
+  apply mf
+  apply mg
+  exact aleb
 
 def FnEven (f : ℝ → ℝ) : Prop :=
   ∀ x, f x = f (-x)
@@ -115,23 +155,41 @@ def FnEven (f : ℝ → ℝ) : Prop :=
 def FnOdd (f : ℝ → ℝ) : Prop :=
   ∀ x, f x = -f (-x)
 
+-- the first step (simplifying the lambda) is necessary
+-- because otherwise rw wouldn't recognize
+-- the fx, gx terms
 example (ef : FnEven f) (eg : FnEven g) : FnEven fun x ↦ f x + g x := by
   intro x
   calc
     (fun x ↦ f x + g x) x = f x + g x := rfl
     _ = f (-x) + g (-x) := by rw [ef, eg]
 
-
 example (of : FnOdd f) (og : FnOdd g) : FnEven fun x ↦ f x * g x := by
-  sorry
+  intro x
+  dsimp
+  calc
+    f x * g x = (-f (-x)) * (-g (-x)) := by rw [of, og]
+    _ = f (-x) * g (-x) := by ring
 
 example (ef : FnEven f) (og : FnOdd g) : FnOdd fun x ↦ f x * g x := by
-  sorry
+  intro x
+  dsimp
+  calc
+    f x * g x = (f (-x)) * (-g (-x)) := by rw [ef, og]
+    _ = -(f (-x) * g (-x)) := by ring
 
+-- dsimp: simp (simplifies the goal), but only for trivial identities
 example (ef : FnEven f) (og : FnOdd g) : FnEven fun x ↦ f (g x) := by
-  sorry
+  intro x
+  dsimp
+  calc
+    f (g (x)) = f (-g (-x)) := by rw [og]
+    _ = f (- - g (-x)) := by rw [ef]
+    _ = f (g (-x)) := by ring_nf
 
 end
+
+-- TODO: ohad: continue from here
 
 section
 
